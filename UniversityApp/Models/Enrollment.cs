04e1cc86cba578345215d2bcd844eb77f3501573
Connor-Karpyszyn <43984978+Connor-Karpyszyn@users.noreply.github.com>
2020-09-22 23:57:43 +0100

    Files upload

---
+++ UniversityApp/Models/Enrollment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UniversityApp.Models
+{
+	public enum Grade
+	{
+		A, B, C, D, F
+	}
+	public class Enrollment
+	{
+		public int EnrollmentID	{ get; set; }
+		public int CourseID { get; set; }
+		public int StudentID { get; set; }
+		public Grade? Grade { get; set; } // ? shows value can be null, meaning student not graded yet
+
+		public virtual Course Course { get; set; } // References the model then needs obj name
+		public virtual Student Student { get; set; }
+	}
+}
---
+++ UniversityApp/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UniversityApp.Models
+{
+	public class Student
+	{
+		public int ID { get; set; } // ID = Primary Key
+		public string LastName { get; set; }
+		[Required]
+		public string FirstName { get; set; }
+		public DateTime EnrollmentDate { get; set; } 
+
+		public virtual ICollection<Enrollment> Enrollments { get; set; } // Navigation Property
+	}
+}
